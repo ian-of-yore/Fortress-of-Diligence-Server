a2b52b67d9e1b112552fb5f9de2ea87dc0ab7785
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-10-25 16:30:33 +0600

    Courses api created

--- index.js
+++ index.js
@@ -18,3 +18,8 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
+
+app.get('/courses/:id', (req, res) => {
+    const selectedCourse = courses.find(course => course.course_id === req.params.id);
+    res.send(selectedCourse);
+})
